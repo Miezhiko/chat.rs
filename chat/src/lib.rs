9dc94a349e5598eae3a302c83e8638d892aa2e67
Miezhiko <Miezhiko@gmail.com>
2023-08-19 11:58:02 +0400

    generate_all method to get results from all the models at once
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- chat/src/lib.rs
+++ chat/src/lib.rs
@@ -48,6 +48,46 @@ pub async fn generate(msg: &str, bot_name: &str, fancy: bool) -> anyhow::Result<
   } else { Err(anyhow!("Failed to generate chat response")) }
 }
 
+pub async fn generate_all<'a>(msg: &str, bot_name: &str, fancy: bool)
+                                -> Vec<(&'a str, anyhow::Result<String>)> {
+  let fmode =
+    if fancy {
+      ! (msg.contains("please")
+      || msg.contains("пожалуйста")
+      || msg.contains("Please")
+      || msg.contains("Пожалуйста")
+      || msg.contains("PLEASE"))
+    } else {
+      false
+    };
+
+  vec![ ( "Chimera llama-2-70b-chat"
+        , chimera::generate( msg, fmode, bot_name, "llama-2-70b-chat" ).await )
+      , ( "gpt-3.5-turbo-16k"
+        , chimera::generate( msg, fmode, bot_name, "gpt-3.5-turbo-16k" ).await )
+      , ( "Deep AI"
+        , g4f::deepai::generate( msg, true, bot_name ).await )
+      , ( "opchatgpts"
+        , g4f::opchatgpts::generate( msg, true, bot_name ).await )
+      , ( "Getgpt"
+        , g4f::getgpt::generate( msg, true, bot_name ).await )
+      , ( "Ails"
+        , g4f::ails::generate( msg, true, bot_name ).await )
+      , ( "Phind"
+        , phind::generate( msg, fmode, bot_name ).await )
+      , ( "Yqcloud"
+        , g4f::yqcloud::generate( msg, true, bot_name ).await )
+      , ( "Theb"
+        , gpt4free::theb::generate( msg ) )
+      , ( "ChatGPT AI"
+        , g4f::chatgpt_ai::generate( msg, fmode, bot_name ) )
+      , ( "gpt4free::gptworldAi"
+        , gpt4free::gptworldAi::generate( msg, fmode, bot_name ).await )
+      , ( "OpenGPT Chatbase"
+        , opengpt::chatbase::generate( msg ) )
+  ]
+}
+
 pub async fn chat(msg: &str, bot_name: &str) -> anyhow::Result<String> {
   generate(msg, bot_name, true).await
 }
