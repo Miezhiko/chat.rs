16acac40eb7182f0d130962ca705bd9b47ceae61
Miezhiko <Miezhiko@gmail.com>
2023-08-30 10:16:21 +0400

    drop broken
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- chat/src/g4f/mod.rs
+++ chat/src/g4f/mod.rs
@@ -1,6 +1,4 @@
 pub mod deepai;
-pub mod opchatgpts;
 pub mod yqcloud;
-pub mod aichat;
 pub mod bloom;
 pub mod wewordle;
--- chat/src/lib.rs
+++ chat/src/lib.rs
@@ -24,11 +24,7 @@ pub async fn generate(msg: &str, bot_name: &str, fancy: bool) -> anyhow::Result<
     Ok(gpt4free_result)
   } else if let Ok(gpt4free_result) = chimera::generate( msg, fmode, bot_name, "llama-2-70b-chat" ).await {
     Ok(gpt4free_result)
-  } else if let Ok(gpt4free_result) = g4f::opchatgpts::generate( msg, fmode, bot_name ).await {
-    Ok(gpt4free_result)
   } else if let Ok(gpt4free_result) = g4f::yqcloud::generate( msg, fmode, bot_name ).await {
-    Ok(gpt4free_result)
-  } else if let Ok(gpt4free_result) = g4f::aichat::generate( msg, fmode, bot_name ).await {
     Ok(gpt4free_result)
   } else if let Ok(gpt4free_result) = g4f::wewordle::generate( msg, fmode, bot_name ).await {
     Ok(gpt4free_result)
@@ -58,19 +54,12 @@ pub async fn generate_all<'a>(msg: &str, bot_name: &str, fancy: bool)
 
   vec![ ( "llama-2-70b-chat"
         , chimera::generate( msg, fmode, bot_name, "llama-2-70b-chat" ).await )
-      , ( "opchatgpts"
-        , g4f::opchatgpts::generate( msg, true, bot_name ).await )
       , ( "Phind"
         , phind::generate( msg, fmode, bot_name ).await )
       , ( "Yqcloud"
         , g4f::yqcloud::generate( msg, true, bot_name ).await )
-      // exclude bloom from multigen because it's very unstable
-      // , ( "Bloom (Vercel)"
-      //  , g4f::bloom::generate( msg, true, bot_name ).await )
       , ( "Wewordle"
         , g4f::wewordle::generate( msg, true, bot_name ).await )
-      , ( "Aichat"
-        , g4f::aichat::generate( msg, true, bot_name ).await )
       , ( "Deep AI"
         , g4f::deepai::generate( msg, true, bot_name ).await )
       , ( "gpt-3.5-turbo-16k"
